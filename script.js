158b17abc23743cbcda7071d19ccaca258bb6dc8
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-27 13:26:39 -0400

    HTML additions, 24hrs time, added hrs, work and lunch icon, save button icon

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+
+$("#currentDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
+
+
+
+
+
+
+
 var createTask = function(taskText, taskDate, taskList) {
     // create elements that make up a task item
     var taskLi 
