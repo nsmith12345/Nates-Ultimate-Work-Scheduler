711594a6e72001d0b7d47a9f5698487f156d1def
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-27 19:49:22 -0400

    display timer

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 $(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-
-$("#currentDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
-
-
-
-
-
-
-
-var createTask = function(taskText, taskDate, taskList) {
-    // create elements that make up a task item
-    var taskLi 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-};
-
-
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks")); 
-    
-    //if nothing in localStorage, create a new object to track all status arrays
-    if (!tasks) {
-        tasks = {
-            toDo: [],
-            inProgress:[],
-            inReview: [],
-            done: []
-        };
-    }
-
-};
-
-
-
-
-$("#task-form-modal .saveBtn").click(function() {
-
-});
+    //display current day & time.
+    $("#currentDay").text(moment().format("DD MMMM YYYY, HHmm:ss"));
+})
