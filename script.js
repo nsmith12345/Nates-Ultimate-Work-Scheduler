0ff722f3d03f1a5002c28e8686a387baa0378895
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-27 20:22:07 -0400

    local storage setup

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-    //display current day & time.
+$(document).ready(function () {
+    //Displays the Current Date and Time in the Header
+
     $("#currentDay").text(moment().format("DD MMMM YYYY, HHmm:ss"));
+    var currentHour = moment().hour(); //Stores the current hour for comparison of each row.
+
+    $(".saveButton").on("Click", function () { //Runs each time a button is clicked and saves the input into "localstorage"
+
+    var workEvent = $(this).siblings(".description").val();
+    var timeStart = $(this).parent().attr("id");
+    localStorage.setItem(timeStart, workEvent);
 })
+
