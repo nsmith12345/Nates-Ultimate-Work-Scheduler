b756f89bd53d178cf47ac07a878aab84b1b08d08
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-27 20:32:45 -0400

    more local storage info

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ $(document).ready(function () {
 
     $(".saveButton").on("Click", function () { //Runs each time a button is clicked and saves the input into "localstorage"
 
-    var workEvent = $(this).siblings(".description").val();
-    var timeStart = $(this).parent().attr("id");
-    localStorage.setItem(timeStart, workEvent);
+        var workEvent = $(this).siblings(".description").val();
+        var timeStart = $(this).parent().attr("id");
+        localStorage.setItem(timeStart, workEvent);
 })
 
+    $(".time-block").each(function () { //Loops through each time-block div to get the information from local storage and determines if it is in the past present or future.
+        var storageHour = ($(this).attr("id")); //stores the ID attribute of the "hour7 div"
+
+        var rowHour = parseInt($(this).attr("id").split("hour")[1]); //Stores the ID div number of the hour div
+        $("#" + storageHour + " .description").val(localStorage.getItem(storageHour));
+        
