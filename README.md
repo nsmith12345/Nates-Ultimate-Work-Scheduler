2eb3b07fb7eaa23d7a9844bb868d43b540bd7d70
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-27 21:39:20 -0400

    updated README (website link)

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Stackpath
 
 Website Link:
 
-
+https://nsmith12345.github.io/Nates-Ultimate-Work-Scheduler/ 
 
 Screen Shot:
 
--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     //Displays the Current Date and Time in the Header
 
     $("#currentDay").text(moment().format("DD MMMM YYYY, HHmm:ss"));
-        var currentHour = moment().hour(); //Stores the current hour for comparison of each row.
+    var currentHour = moment().hour(); //Stores the current hour for comparison of each row.
 
-    $(".saveButton").on("Click", function () { //Runs each time a button is clicked and saves the input into "localstorage"
+    $(".saveBtn").on("click", function () { //Runs each time a button is clicked and saves the input into "localstorage"
 
         var workEvent = $(this).siblings(".description").val();
         var timeStart = $(this).parent().attr("id");
@@ -17,20 +17,20 @@ $(document).ready(function () {
         var rowHour = parseInt($(this).attr("id").split("hour")[1]); //Stores the ID div number of the hour div
         $("#" + storageHour + " .description").val(localStorage.getItem(storageHour));
     
-    if (rowHour < currentHour) { //Is current Block in the Past?
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-    }
-    else if (rowHour === currenthour) { //Is this the current Block?
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-    }
-    else { //All remaining blocks are in the future.
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-    }
+        if (rowHour < currentHour) { //Is current Block in the Past?
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (rowHour === currenthour) { //Is this the current Block?
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else { //All remaining blocks are in the future.
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
   })
 })
